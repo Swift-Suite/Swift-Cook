bf4b3fb3cd658b4988d36fbf97f1ce81b0f5c67f
sbuchmiller <scottbuchmiller@gmail.com>
2020-09-17 00:04:25 -0700

    changed default app to just be a placeholder widget that shows text

---
+++ lib/recipeView.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class RecipeViewPage extends StatefulWidget{
+
+  @override
+  RecipeViewState createState() => RecipeViewState();
+  
+}
+
+class RecipeViewState extends State<RecipeViewPage>{
+
+  @override
+  Widget build(BuildContext context){
+    return Text("test");
+  }
+
+}
