f33fb2d4b7eefce4f6525b6194bef685926e3351
Jason <jasoz10@uci.edu>
2020-09-26 23:37:52 -0700

    Popupmenu added
    added a popup menu to recipe_cards

--- lib/Pages/recipes_page_widgets/recipe_card.dart
+++ lib/Pages/recipes_page_widgets/recipe_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:swiftcook/model/recipe.dart';
 
-class RecipeCard extends StatelessWidget{
+enum menuItems { one, two, three }
+
+class RecipeCard extends StatelessWidget {
   final Recipe recipe;
   final ValueChanged<Recipe> recipeSelectedCallback;
   //final Widget thumbnail;
@@ -11,57 +13,98 @@ class RecipeCard extends StatelessWidget{
     //this.thumbnail,
     this.recipe,
     this.recipeSelectedCallback,
-  }) : super(key:key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical:10.0),
-      child: Container(
-        height: size.height/8,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.lightBlueAccent,
-        ),
-        child: RawMaterialButton(
-          //fillColor: Colors.lightBlueAccent,
-            onPressed: () => recipeSelectedCallback(recipe),
-          child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget> [
-                AspectRatio(
-                    aspectRatio: 1.0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    )
-
-                ),
-                Expanded(
-                    child: Padding(
-                        padding: const EdgeInsets.fromLTRB(20.0,0.0,2.0,0.0),
-                        child: Center(
-                            child: Text(recipe.title, textAlign: TextAlign.center))
-                    )
-                )
-              ]
-          )
-        )
-
-      )
-    );
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Container(
+            height: size.height / 8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.lightBlueAccent,
+            ),
+            //onPressed: () => recipeSelectedCallback(recipe),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  RawMaterialButton(
+                    onPressed: () => recipeSelectedCallback(recipe),
+                    child: AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: const DecorationImage(
+                              image: NetworkImage(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        )),
+                  ),
+                  Expanded(
+                      child: RawMaterialButton(
+                          onPressed: () => recipeSelectedCallback(recipe),
+                          child: Center(
+                              child: Text(recipe.title,
+                                  textAlign: TextAlign.center)))),
+                  VerticalDivider(),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: PopupMenuButton<menuItems>(
+                        icon: Icon(Icons.more_vert),
+                        onSelected: null,
+                        itemBuilder: (BuildContext context) =>
+                            <PopupMenuEntry<menuItems>>[
+                              const PopupMenuItem(
+                                  value: menuItems.one, child: Text("one")),
+                              const PopupMenuItem(
+                                  value: menuItems.two, child: Text("two")),
+                              const PopupMenuItem(
+                                  value: menuItems.three, child: Text("three")),
+                            ]),
+                  ),
+                ])));
   }
-
-
-
 }
+
+// class DropDownMenu extends StatefulWidget {
+//   DropDownMenu({Key key}) : super(key: key);
+//
+//   @override
+//   _DropDownMenuState createState() => _DropDownMenuState();
+// }
+//
+// class _DropDownMenuState extends State<DropDownMenu>{
+//   String dropdownValue = 'One';
+//   @override
+//   Widget build(BuildContext context){
+//     return DropdownButton<String>(
+//       icon: Icon(Icons.more_vert),
+//       iconSize: 24,
+//       elevation: 16,
+//       style: TextStyle(color: Colors.deepPurple),
+//       onChanged: (String newValue) {
+//         setState(() {
+//           dropdownValue = newValue;
+//         });
+//       },
+//       items: <String>['One', 'Two', 'Free', 'Four']
+//           .map<DropdownMenuItem<String>>((String value) {
+//         return DropdownMenuItem<String>(
+//           value: value,
+//           child: Text(value),
+//         );
+//       }).toList(),
+//     );
+//   }
+//
+// }
