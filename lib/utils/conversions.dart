9ce468d332e6038a7b90cd986d0dfe42b99b6706
drossmei <drossmei@uci.edu>
2020-09-27 17:47:46 -0700

    DR - made class map private and created return for Iterable of all unit types

--- lib/utils/conversions.dart
+++ lib/utils/conversions.dart
@@ -13,7 +13,7 @@ class Converter {
  *     Output: "US_teaspoon"
  */
 
-  Map<String, double> unitsToML = Map<String, double>();
+  Map<String, double> _unitsToML = Map<String, double>();
   String usTsp = 'US_teaspoon';
   String ukTsp = 'UK_teaspoon';
   String metricTsp = 'Metric_teaspoon';
@@ -38,35 +38,35 @@ class Converter {
 
   Converter() {
     // Volume
-    unitsToML['US_teaspoon'] = 4.9289215937;
-    unitsToML['UK_teaspoon'] = 5.9193880208;
-    unitsToML['Metric_teaspoon'] = 5;
+    _unitsToML['US_teaspoon'] = 4.9289215937;
+    _unitsToML['UK_teaspoon'] = 5.9193880208;
+    _unitsToML['Metric_teaspoon'] = 5;
 
-    unitsToML['US_tablespoon'] = 14.786764781;
-    unitsToML['UK_tablespoon'] = 17.758164063;
-    unitsToML['Metric_tablespoon'] = 15;
+    _unitsToML['US_tablespoon'] = 14.786764781;
+    _unitsToML['UK_tablespoon'] = 17.758164063;
+    _unitsToML['Metric_tablespoon'] = 15;
 
-    unitsToML['US_cup'] = 236.5882365;
-    unitsToML['UK_cup'] = 284.130625;
-    unitsToML['Metric_cup'] = 250;
+    _unitsToML['US_cup'] = 236.5882365;
+    _unitsToML['UK_cup'] = 284.130625;
+    _unitsToML['Metric_cup'] = 250;
 
-    unitsToML['US_pint'] = 473.176473;
-    unitsToML['UK_pint'] = 568.26125;
+    _unitsToML['US_pint'] = 473.176473;
+    _unitsToML['UK_pint'] = 568.26125;
 
-    unitsToML['US_quarts'] = 946.352946;
-    unitsToML['UK_quarts'] = 1136.5225;
+    _unitsToML['US_quarts'] = 946.352946;
+    _unitsToML['UK_quarts'] = 1136.5225;
 
-    unitsToML['US_gallon'] = 3785.411784;
-    unitsToML['UK_gallon'] = 4546.09;
+    _unitsToML['US_gallon'] = 3785.411784;
+    _unitsToML['UK_gallon'] = 4546.09;
 
-    unitsToML['US_barrel'] = 119240.4712;
-    unitsToML['UK_barrel'] = 163659.24;
+    _unitsToML['US_barrel'] = 119240.4712;
+    _unitsToML['UK_barrel'] = 163659.24;
 
-    unitsToML['US_fl_ounce'] = 29.573529562;
-    unitsToML['UK_fl_ounce'] = 28.4130625;
+    _unitsToML['US_fl_ounce'] = 29.573529562;
+    _unitsToML['UK_fl_ounce'] = 28.4130625;
 
-    unitsToML['mL'] = 1;
-    unitsToML['liter'] = 1000;
+    _unitsToML['mL'] = 1;
+    _unitsToML['liter'] = 1000;
   }
 
   double convert(double quantity, String fromUnit, String toUnit) {
@@ -80,6 +80,12 @@ class Converter {
    *       OR
    *      double amount = converter.convert('US_teaspoon', 'UK_barrel');
    */
-    return quantity * unitsToML[fromUnit] / unitsToML[toUnit];
+    return quantity * _unitsToML[fromUnit] / _unitsToML[toUnit];
+  }
+
+  // Keys returns an iterable of the dictionary, haven't tested this so I'm not
+  // sure if it works
+  Iterable<String> getUnits() {
+    return _unitsToML.keys;
   }
 }
