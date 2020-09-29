a3cb0ac55ec54c7f8dade60129f55a7bd2c288dc
sbuchmiller <scottbuchmiller@gmail.com>
2020-09-28 22:32:38 -0700

    added SQL stuff to instruction and reworked database-test

--- test/database_test.dart
+++ test/database_test.dart
@@ -2,12 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:swiftcook/model/data_objects/ingredient.dart';
+import 'package:swiftcook/model/data_objects/instruction.dart';
 import 'package:swiftcook/swift_exception.dart';
 
 import '../lib/model/database_manager.dart';
 
 void main() {
-  test('database should load', () async {
+  ingredientTest();
+  instructionTest();
+}
+
+void ingredientTest() async {
     WidgetsFlutterBinding.ensureInitialized();
 
     bool error = false;
@@ -18,14 +23,14 @@ void main() {
     try {
       Ingredient ingredient = new Ingredient(0, "Curry", 10, "grams");
 
-      print("====== testing dbInsert ======");
+      print("====== testing dbInsert Ingredient ======");
       int result = await ingredient.dbInsert();
       print(result);
 
       var list = await Ingredient.retrieveAll();
       print(list);
 
-      print("====== testing dbUpdate ======");
+      print("====== testing dbUpdate Ingredient ======");
       ingredient.title = "Paste";
       ingredient.quantity = 15;
       ingredient.unit = "lbs";
@@ -33,7 +38,7 @@ void main() {
       list = await Ingredient.retrieveAll();
       print(list);
 
-      print("====== testing dbDelete ======");
+      print("====== testing dbDelete Ingredient ======");
       bool deleteResult = await ingredient.dbDelete();
       if (!deleteResult) throw Exception("delete did not return true");
     } catch (e) {
@@ -46,5 +51,42 @@ void main() {
     }
 
     expect(error, false);
-  });
+}
+
+void instructionTest() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    bool error = false;
+
+    Database db = await DatabaseManager.instance.database;
+    db.rawDelete("DELETE FROM Instruction WHERE rowid > 0");
+
+    try {
+      Instruction instruction = new Instruction(0, "flip the borgar");
+
+      print("====== testing dbInsert Instruction ======");
+      int result = await instruction.dbInsert();
+      print(result);
+
+      var list = await Ingredient.retrieveAll();
+      print(list);
+
+      print("====== testing dbUpdate Instruction ======");
+      instruction.content = "Toucha the spaghet";
+      bool updateResult = await instruction.dbUpdate();
+      list = await Ingredient.retrieveAll();
+      print(list);
+
+      print("====== testing dbDelete Instruction ======");
+      bool deleteResult = await instruction.dbDelete();
+      if (!deleteResult) throw Exception("delete did not return true");
+    } catch (e) {
+      if (e is BaseException) {
+        print(e.cause);
+      }
+
+      print(e);
+      error = true;
+    }
+    expect(error, false);
 }
