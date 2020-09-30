09c630ae8a4b34bc008abec284859492a8f1755a
drossmei <drossmei@uci.edu>
2020-09-29 17:05:47 -0700

    Updated mapped names for converter

--- lib/utils/converter.dart
+++ lib/utils/converter.dart
@@ -20,60 +20,60 @@ class Converter {
   }
   Converter._internal() {
     // Volume
-    _unitsToML['US_teaspoon'] = 4.9289215937;
-    _unitsToML['UK_teaspoon'] = 5.9193880208;
-    _unitsToML['Metric_teaspoon'] = 5;
+    _unitsToML['US Teaspoon'] = 4.9289215937;
+    _unitsToML['UK Teaspoon'] = 5.9193880208;
+    _unitsToML['Metric Teaspoon'] = 5;
 
-    _unitsToML['US_tablespoon'] = 14.786764781;
-    _unitsToML['UK_tablespoon'] = 17.758164063;
-    _unitsToML['Metric_tablespoon'] = 15;
+    _unitsToML['US Tablespoon'] = 14.786764781;
+    _unitsToML['UK Tablespoon'] = 17.758164063;
+    _unitsToML['Metric Tablespoon'] = 15;
 
-    _unitsToML['US_cup'] = 236.5882365;
-    _unitsToML['UK_cup'] = 284.130625;
-    _unitsToML['Metric_cup'] = 250;
+    _unitsToML['US Cup'] = 236.5882365;
+    _unitsToML['UK Cup'] = 284.130625;
+    _unitsToML['Metric Cup'] = 250;
 
-    _unitsToML['US_pint'] = 473.176473;
-    _unitsToML['UK_pint'] = 568.26125;
+    _unitsToML['US Pint'] = 473.176473;
+    _unitsToML['UK Pint'] = 568.26125;
 
-    _unitsToML['US_quarts'] = 946.352946;
-    _unitsToML['UK_quarts'] = 1136.5225;
+    _unitsToML['US Quart'] = 946.352946;
+    _unitsToML['UK Quart'] = 1136.5225;
 
-    _unitsToML['US_gallon'] = 3785.411784;
-    _unitsToML['UK_gallon'] = 4546.09;
+    _unitsToML['US Gallon'] = 3785.411784;
+    _unitsToML['UK Gallon'] = 4546.09;
 
-    _unitsToML['US_barrel'] = 119240.4712;
-    _unitsToML['UK_barrel'] = 163659.24;
+    _unitsToML['US Barrel'] = 119240.4712;
+    _unitsToML['UK Barrel'] = 163659.24;
 
-    _unitsToML['US_fl_ounce'] = 29.573529562;
-    _unitsToML['UK_fl_ounce'] = 28.4130625;
+    _unitsToML['US Fl Ounce'] = 29.573529562;
+    _unitsToML['UK Fl Ounce'] = 28.4130625;
 
-    _unitsToML['mL'] = 1;
-    _unitsToML['liter'] = 1000;
+    _unitsToML['Milliliter'] = 1;
+    _unitsToML['Liter'] = 1000;
   }
 
   // Class Public Variables
   Map<String, double> _unitsToML = Map<String, double>();
-  String usTsp = 'US_teaspoon';
-  String ukTsp = 'UK_teaspoon';
-  String metricTsp = 'Metric_teaspoon';
-  String usTbsp = 'US_tablespoon';
-  String ukTbsp = 'UK_tablespoon';
-  String metricTbsp = 'Metric_tablespoon';
-  String usCup = 'US_cup';
-  String ukCup = 'UK_cup';
-  String metricCup = 'Metric_cup';
-  String usPint = 'US_pint';
-  String ukPint = 'UK_pint';
-  String usQuart = 'US_quart';
-  String ukQuart = 'UK_quart';
-  String usGallon = 'US_gallon';
-  String ukGallon = 'UK_gallon';
-  String usBarrel = 'US_barrel';
-  String ukBarrel = 'UK_barrel';
-  String usFlOunce = 'US_fl_ounce';
-  String ukFlOunce = 'UK_fl_ounce';
-  String mL = 'mL';
-  String liter = 'liter';
+  String usTsp = 'US Teaspoon';
+  String ukTsp = 'UK Teaspoon';
+  String metricTsp = 'Metric Teaspoon';
+  String usTbsp = 'US Tablespoon';
+  String ukTbsp = 'UK Tablespoon';
+  String metricTbsp = 'Metric Tablespoon';
+  String usCup = 'US Cup';
+  String ukCup = 'UK Cup';
+  String metricCup = 'Metric Cup';
+  String usPint = 'US Pint';
+  String ukPint = 'UK Pint';
+  String usQuart = 'US Quart';
+  String ukQuart = 'UK Quart';
+  String usGallon = 'US Gallon';
+  String ukGallon = 'UK Gallon';
+  String usBarrel = 'US Barrel';
+  String ukBarrel = 'UK Barrel';
+  String usFlOunce = 'US Fl Ounce';
+  String ukFlOunce = 'UK Fl Ounce';
+  String mL = 'Milliliter';
+  String liter = 'Liter';
 
   double convert(double quantity, String fromUnit, String toUnit) {
     /**
