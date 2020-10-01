709381684fd3f1c647d152658226157832eb5fb8
Jason <jasoz10@uci.edu>
2020-09-30 22:33:54 -0700

    JZ added slide to delete.
    Swipe from the right to delete an item in the list. Currently does not get rid of the "divider bar" though

--- lib/Pages/edit_page.dart
+++ lib/Pages/edit_page.dart
@@ -73,6 +73,13 @@ class _EditPageState extends State<EditPage> {
         _controllers.add(IngredientController(widget.recipe.id,TextEditingController(), "", TextEditingController(), "1.0" .toString(), "ml"));
       });
     }
+    void removeIngredient(int index){
+      print("removing");
+      setState((){
+        ingredientList.removeAt(index);
+        _controllers.removeAt(index);
+      });
+    }
 
     //print(serializables.length);
     return Scaffold(
@@ -119,7 +126,8 @@ class IngredientRow extends StatefulWidget {
   final Size size;
   final ingredientController;
   final int index;
-  IngredientRow({Key key,this.ingredientController,this.index, @required this.size}) : super(key: key);
+  final Function removeAt;
+  IngredientRow({Key key,this.ingredientController,this.index, this.size, this.removeAt}) : super(key: key);
   @override
   _IngredientRowState createState() => _IngredientRowState();
 }
@@ -133,61 +141,77 @@ class _IngredientRowState extends State<IngredientRow> {
   @override
   Widget build(BuildContext context) {
     String dropDownValue = widget.ingredientController.unit;
-    return Row(
-      children:[
-        Container(
-          child: Icon(Icons.edit, size:15),
-          width: widget.size.width/12,
-        ),
-        Expanded(
-          child: TextFormField(
-            controller: widget.ingredientController.nameController,
-            decoration: new InputDecoration(
-              border: OutlineInputBorder(),
-            ),
-            style: TextStyle(fontSize: 10),
-            //controller: widget.controller,
-          )
-        ),
-        Container( //quanity
-          padding: const EdgeInsets.symmetric(vertical:5, horizontal:5),
-          child: TextFormField(
-            keyboardType: TextInputType.number,
-              controller: widget.ingredientController.quantityController,
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color:Colors.red,
+        child: Icon(Icons.cancel),
+      ),
+      //secondaryBackground: Container(
+        //color: Colors.red,
+        //child: Icon(Icons.cancel)
+      //),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction){
+        widget.removeAt(widget.index);
+      },
+      child: Row(
+        children:[
+          Container(
+            child: Icon(Icons.edit, size:15),
+            width: widget.size.width/12,
+          ),
+          Expanded(
+            child: TextFormField(
+              controller: widget.ingredientController.nameController,
               decoration: new InputDecoration(
                 border: OutlineInputBorder(),
               ),
-            style: TextStyle(fontSize: 10),
+              style: TextStyle(fontSize: 10),
+              //controller: widget.controller,
+            )
           ),
-          width: widget.size.width/8
-
-        ),
-        Container( //unit of measure
+          Container( //quanity
             padding: const EdgeInsets.symmetric(vertical:5, horizontal:5),
-            child: DropdownButton<String>(
-              value:dropDownValue,
-              //icon: Icon(Icons.more_vert),
-              elevation: 16,
-              style: TextStyle(color: Colors.black,fontSize: 10),
-              onChanged: (String newValue) {
-                setState(() {
-                  dropDownValue = newValue;
-                  widget.ingredientController.unit = newValue;
-                });
-              },
-              items: <String>['ml','L', 'C', 'g', 'kg','lb','oz', 'pt', 't','T']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value, style: TextStyle(fontSize:10)),
-                );
-              }).toList(),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+                controller: widget.ingredientController.quantityController,
+                decoration: new InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              style: TextStyle(fontSize: 10),
             ),
             width: widget.size.width/8
 
+          ),
+          Container( //unit of measure
+              padding: const EdgeInsets.symmetric(vertical:5, horizontal:5),
+              child: DropdownButton<String>(
+                value:dropDownValue,
+                //icon: Icon(Icons.more_vert),
+                elevation: 16,
+                style: TextStyle(color: Colors.black,fontSize: 10),
+                onChanged: (String newValue) {
+                  setState(() {
+                    dropDownValue = newValue;
+                    widget.ingredientController.unit = newValue;
+                  });
+                },
+                items: <String>['ml','L', 'C', 'g', 'kg','lb','oz', 'pt', 't','T']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value, style: TextStyle(fontSize:10)),
+                  );
+                }).toList(),
+              ),
+              width: widget.size.width/8
 
-        ),
-      ]
+          ),
+          Container()
+
+        ]
+      ),
     );
   }
 }
